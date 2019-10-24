b53ec47951043eaf4b5df1c489c0f26d22a4ac6f
naqashnvd <naqashnvd@hotmail.com>
2019-10-24 18:24:41 +0500

    ok

--- verilog/riscV/riscv.v
+++ verilog/riscV/riscv.v
@@ -11,21 +11,21 @@ wire clock = KEY[0];
 wire clear = KEY[1];
 
 
-assign LEDR[9:0]=signals[9:0];
+assign LEDR[9:0]=dataA[9:0];
 
 
 register pc(
 	.data(pcIn),
 	.enable(1'b1),
-	.clock(~clock),
+	.clock(clock),
 	.clear(clear),
 	.out(imemAddr)
 );
 
 
 always@(*)begin
-	if(branchFromAlu&signals[7]) pcIn = imemAddr+immGenOut;
-	else pcIn = imemAddr+4;
+	if(branchFromAlu&signals[7]) pcIn = (imemAddr+(immGenOut >>> 2));
+	else pcIn = imemAddr+1;
 end
 
 
@@ -106,32 +106,33 @@ end
 
 endmodule
 
+
+
 //Instruction Memory
-module IRAM #(parameter width = 8,parameter addrWidth = 8)(
-output [(width*4)-1:0] DOUT,
-input [addrWidth-1:0] ADDR,
-input [(width*4)-1:0] DIN,
+module IRAM #(parameter width = 32,parameter addrWidth = 8)(
+output reg [(width)-1:0] DOUT,
+input [8-1:0] ADDR,
+input [(width)-1:0] DIN,
 input wren, clear, clk
 );
-reg [width-1:0] MEM [2**(addrWidth)-1:0];
-
-
-
+reg [width-1:0] MEM [2**(8)-1:0];
 integer i;
 initial begin
-for(i=0; i<(2**(addrWidth)-1); i=i+1)
-	MEM[i] = 8'b0;
-//{MEM[3],MEM[2],MEM[1],MEM[0]}=32'h00002083;
-//{MEM[7],MEM[6],MEM[5],MEM[4]}=32'h00402103;
 
-// {MEM[3],MEM[2],MEM[1],MEM[0]}=32'h00a00093;
-// {MEM[7],MEM[6],MEM[5],MEM[4]}=32'h01400113;
-// {MEM[11],MEM[10],MEM[9],MEM[8]}=32'h00102023;
-// {MEM[15],MEM[14],MEM[13],MEM[12]}=32'h00202223;
-// {MEM[19],MEM[18],MEM[17],MEM[16]}=32'h00002183;
-// {MEM[23],MEM[22],MEM[21],MEM[20]}=32'h00402203;
-// {MEM[27],MEM[26],MEM[25],MEM[24]}=32'h004182b3;
-// //{MEM[31],MEM[30],MEM[29],MEM[28]}=32'h004182b3;
+for(i=0; i<(2**(8)-1); i=i+1)
+	MEM[i] = 32'b0;
+
+//MEM[0]=32'h00002083;
+//MEM[1]=32'h00402103;
+//
+// MEM[0]=32'h00a00093;
+// MEM[1]=32'h01400113;
+// MEM[2]=32'h00102023;
+// MEM[3]=32'h00202223;
+// MEM[4]=32'h00002183;
+// MEM[5]=32'h00402203;
+// MEM[7]=32'h004182b3;
+
 
 
 
@@ -140,43 +141,46 @@ for(i=0; i<(2**(addrWidth)-1); i=i+1)
 // bne x1,x2,true
 // addi x3,x0,1
 // true:
-// addi x4,x0,1
- {MEM[3],MEM[2],MEM[1],MEM[0]}=32'h00100093; 
- {MEM[7],MEM[6],MEM[5],MEM[4]}=32'h00200113; 
- {MEM[11],MEM[10],MEM[9],MEM[8]}=32'h00209463; 
- {MEM[15],MEM[14],MEM[13],MEM[12]}=32'h00100193;
- {MEM[19],MEM[18],MEM[17],MEM[16]}=32'h00100213;
+//// addi x4,x0,1
+MEM[0]=32'h00100093; 
+MEM[1]=32'h00200113; 
+MEM[2]=32'h00209463; 
+MEM[3]=32'h00100193;
+MEM[4]=32'h00100213;
 
 
 
 //Test Codes
-// {MEM[3],MEM[2],MEM[1],MEM[0]}=32'h00800093;
-// {MEM[7],MEM[6],MEM[5],MEM[4]}=32'h03100293;
-// {MEM[11],MEM[10],MEM[9],MEM[8]}=32'h00500333;
-// {MEM[15],MEM[14],MEM[13],MEM[12]}=32'h00120213;
-// {MEM[19],MEM[18],MEM[17],MEM[16]}=32'hfe415ce3;
-// {MEM[23],MEM[22],MEM[21],MEM[20]}=32'h00000213;
-// {MEM[27],MEM[26],MEM[25],MEM[24]}=32'h00128293;
-// {MEM[31],MEM[30],MEM[29],MEM[28]}=32'h00110113;
-// {MEM[35],MEM[34],MEM[33],MEM[32]}=32'hfe20d4e3;
+// MEM[0]=32'h00800093;
+// MEM[1]=32'h03100293;
+// MEM[2]=32'h00500333;
+// MEM[3]=32'h00120213;
+// MEM[4]=32'hfe415ce3;
+// MEM[5]=32'h00000213;
+// MEM[6]=32'h00128293;
+// MEM[7]=32'h00110113;
+// MEM[8]=32'hfe20d4e3;
 
 
 end
 
-always @(posedge clk) begin
-if(wren)
-{MEM[ADDR+3],MEM[ADDR+2],MEM[ADDR+1],MEM[ADDR]} <= DIN;
+always @(posedge clk or negedge clear) begin
+if(~clear) DOUT <=32'b0;
+else begin 
+	DOUT <= MEM[ADDR];
+	if(wren) MEM[ADDR] <= DIN;
 end
-assign DOUT = {MEM[ADDR+3],MEM[ADDR+2],MEM[ADDR+1],MEM[ADDR]};
+end
+//assign DOUT = MEM[ADDR];
 
 endmodule
 
 
 //Data Memory
-module DataRAM #(parameter width = 8,parameter addrWidth = 8)(
-output [(width*4)-1:0] DOUT,
+module DataRAM #(parameter width = 32,parameter addrWidth = 8)(
+output reg [(width)-1:0] DOUT,
 input [addrWidth-1:0] ADDR,
-input [(width*4)-1:0] DIN,
+input [(width)-1:0] DIN,
 input wren, clear, clk
 );
 reg [width-1:0] MEM [2**(addrWidth)-1:0];
@@ -184,17 +188,21 @@ reg [width-1:0] MEM [2**(addrWidth)-1:0];
 integer i;
 initial begin
 for(i=0; i<(2**(addrWidth)-1); i=i+1)
-	MEM[i] = 8'b0;
-//{MEM[3],MEM[2],MEM[1],MEM[0]}=32'h1;
-//{MEM[7],MEM[6],MEM[5],MEM[4]}=32'h2;
+	MEM[i] = 32'b0;
+//MEM[0]=32'h1;
+//MEM[1]=32'h2;
 
 end
 
-always @(posedge clk ) begin
-if(wren) {MEM[ADDR+3],MEM[ADDR+2],MEM[ADDR+1],MEM[ADDR]} <= DIN;
+always @(posedge clk or negedge clear) begin
+if(~clear) DOUT <=32'b0;
+else begin 
+	DOUT <= MEM[ADDR];
+	if(wren) MEM[ADDR] <= DIN;
+end
 end
 
-assign DOUT = {MEM[ADDR+3],MEM[ADDR+2],MEM[ADDR+1],MEM[ADDR]};
+//assign DOUT = MEM[ADDR];
 
 endmodule
 
