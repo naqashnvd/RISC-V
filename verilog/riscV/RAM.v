7f2560d4a4e358212870be4bc09e1e477e96c530
naqashnvd <naqashnvd@hotmail.com>
2019-11-12 03:30:09 +0500

    bugs fixed

---
+++ verilog/riscV/RAM.v
@@ -0,0 +1,20 @@
+
+module RAM #(parameter width = 32,parameter addrWidth = 8)(
+output [(width)-1:0] DOUT,
+input [addrWidth-1:0] ADDR,
+input [(width)-1:0] DIN,
+input wren, clock
+);
+reg [width-1:0] MEM [2**(addrWidth)-1:0];
+
+integer i;
+initial begin
+	for(i=0;i<2**(addrWidth);i=i+1) MEM[i]=0;
+end
+
+always @(posedge clock) begin
+	if(wren) MEM[ADDR] <= DIN;
+end
+assign DOUT = MEM[ADDR];
+endmodule
+
